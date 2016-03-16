1d973a058fd88259b1715e6351512eff13aa68d3
Lincoln Fraley <lincoln.fraley93@gmail.com>
2016-03-16 13:22:37 -0400

    updated RotateMatrix

--- RotateMatrix.java
+++ RotateMatrix.java
@@ -18,21 +18,21 @@ public class RotateMatrix {
 				}
 		}
 	
-	public static void main(String[] args) {
-			int[][] array = { {1,2, 3},{4, 5, 6}, {7, 8, 9} };
-			for (int i = 0; i < array.length; i++) {
-					for (int j = 0; j < array.length; j++) {
-							System.out.print(array[i][j] + ",");
-					}
-					System.out.println();
-			}
-			System.out.println();
-			rotateMatrix(array);
-			for (int i = 0; i < array.length; i++) {
-					for (int j = 0; j < array.length; j++) {
-							System.out.print(array[i][j] + ",");
-					}
-					System.out.println();
-			}
-	}
+		public static void main(String[] args) {
+				int[][] array = { {1,2, 3},{4, 5, 6}, {7, 8, 9} };
+				for (int i = 0; i < array.length; i++) {
+						for (int j = 0; j < array.length; j++) {
+								System.out.print(array[i][j] + ",");
+						}
+						System.out.println();
+				}
+				System.out.println();
+				rotateMatrix(array);
+				for (int i = 0; i < array.length; i++) {
+						for (int j = 0; j < array.length; j++) {
+								System.out.print(array[i][j] + ",");
+						}
+						System.out.println();
+				}
+		}
 }
