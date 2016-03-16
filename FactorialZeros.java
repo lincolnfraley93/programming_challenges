f37b62a59483f40f6da36e35d4a6f5bcdf9067a4
Lincoln Fraley <lincoln.fraley93@gmail.com>
2016-03-16 12:48:56 -0400

    added FactorialZeros

---
+++ FactorialZeros.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ * Write a function which computes the number of trailing zeros in
+ * n!.
+ * 
+ * @author Lincoln Fraley
+ * January 16, 2016
+ */
+public class FactorialZeros  {
+		public static int zeros(int n) {
+				int zeros = 0;
+				if (n/5>=0) {
+						zeros=(n/5);
+				}
+				return zeros;
+		}
+	
+	public static void main(String[] args) {
+			long n = 1;
+			for (int i=1; i<=20; i++) {
+					n=n*i;
+					System.out.format("%d!: %d; number of zeros: %d%n", i, n, zeros(i));
+//					System.out.print(i + " factorial: " + n + ", ");
+//					System.out.println("num of zeros: " + zeros(i));
+			}
+	}
+
+}
